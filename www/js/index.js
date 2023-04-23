5c370a143ba767cc3be1769bad2896b3f537a114
Neil Stevens <neil@stevensmedia.us>
2023-04-23 15:10:13 -0400

    ATtempting to use Cordova file access, but it's not working

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,23 @@
-function onDeviceReady() {
-	window.removeMenu();
-
-	console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+function initializeFS() {
+	return new Promise((res, rej) => {
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, res, rej)
+	})
 }
-document.addEventListener('deviceready', onDeviceReady, false);
+
+async function main() {
+	try {
+		await initializeFS()
+	} catch(e) {
+		console.log(e)
+		alert("Unable to load filesystem. Things may fail to work.")
+	}
+
+	const q = (a) => document.querySelector(a)
+	const qa = (a) => document.querySelectorAll(a)
+
+	q("fluent-button[scan]").addEventListener("click", () => {
+		const target = q("#music-path")
+		const path = target.value
+	})
+}
+document.addEventListener('deviceready', main, false);
